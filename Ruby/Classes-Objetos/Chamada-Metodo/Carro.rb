68c7274790f5f3b993aac74594200d0aed64c2c5
aribeiro26 <alexetec.ribeiro@gmail.com>
2021-05-08 17:38:15 -0300

    Metodo

--- Ruby/Classes-Objetos/Chamada-Metodo/Carro.rb
+++ Ruby/Classes-Objetos/Chamada-Metodo/Carro.rb
@@ -1,7 +1,9 @@
-class Carro
+class Carro # Define o template
 
-    def ligarMotor
-        puts ("VRUM VRUM!")
-    end
+    def ligarMotor # Nome do Método
+        puts ("VRUM VRUM!") # Comando que exibe um texto
+    end # Final do Método
+end # Final da classe
 
-end
+fusca = Carro.new # Instanciação do objeto fusca usando a classe carro
+fusca.ligarMotor # Chamando o método do objeto
